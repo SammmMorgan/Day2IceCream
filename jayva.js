00c3d049e0d134c6c6df64f0a3d9e0effc941de4
Sam Morgan <sam2morgan99@gmail.com>
2024-01-30 14:38:54 -0700

    variables and html setup

---
+++ jayva.js
@@ -0,0 +1,50 @@
+// console.log('shit works');
+
+const iceCream = [{
+    name: 'Cookie Dough',
+    price: 1.25,
+    quantity: 0
+},
+{
+    name: 'Vanilla',
+    price: 1,
+    quantity: 0
+},
+{
+    name: 'Strawberry',
+    price: 1.25,
+    quantity: 0
+}]
+
+const vessels = [
+    {
+        name: 'Waffle Cone',
+        price: .75,
+        quantity: 0
+    },
+    {
+        name: 'Sugar Cone',
+        price: .25,
+        quantity: 0
+    },
+    {
+        name: 'Basic Bitch Cup',
+        price: .00,
+        quantity: 0
+    }
+]
+const toppings = [{
+    name: 'Sprinkles',
+    price: .25,
+    quantity: 0,
+},
+{
+    name: 'Chocolate Chips',
+    price: .25,
+    quantity: 0
+},
+{
+    name: 'Gummy Worms',
+    price: .5,
+    quantity: 0
+}]
